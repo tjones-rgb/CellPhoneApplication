6d26bef4c45ec6153d01bf07d5d4c9515954f8b3
Tjones <taouris.jones@gmail.com>
2025-10-01 10:09:04 -0400

    commit

---
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -0,0 +1,61 @@
+package com.pluralsight;
+
+public class CellPhone {
+
+
+      private  double  serialNumber = (1000000);
+     private   String model = ("galaxyS25");
+      private   String carrier = ("tmMobile");
+       private String phoneNumber = ();
+      private  String owner = ("ToddSings");
+
+    public CellPhone(String toddSings) {
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+    }
+
+
+    public double getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(double serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber (String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
+
---
+++ src/main/java/com/pluralsight/CellPhoneService.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class CellPhoneService {
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+      StringBuilder sb = new StringBuilder();
+
+        CellPhone cell = new CellPhone("ToddSings");
+        System.out.println("Cellphone: + ");
+
+
+
+
+
+
+    }
+}
